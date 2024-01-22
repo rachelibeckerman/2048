9a220a3e5d2bffaddca7730eebe8b78d8ed031e7
rachelibeckerman <Hand4c3@mbjcomp.org.il>
2024-01-22 18:24:03 +0200

    finish fix sign up

--- project2024/project2024-2/src/Components/Info/Info.jsx
+++ project2024/project2024-2/src/Components/Info/Info.jsx
@@ -1,43 +1,39 @@
 import React from "react";
-import {useContext} from "react"
+import { useContext } from "react"
 import { Router, useLocation } from "react-router-dom";
 import { appContax } from "../../App";
 
 function Info() {
-    const {user, setUser} = useContext(appContax);
+    const { user, setUser } = useContext(appContax);
+
+    const printUser = (object) => {
+
+
+        return Object.keys(object).map((key) => (typeof object[key] === 'object' ?
+            <div key={key} >
+                <br />
+                <p>
+                    <ins>
+                        <strong>{key + ":"}</strong>
+                    </ins>
+                </p>
+                {printUser(object[key])}
+            </div> :
+            <p key={key}>
+                <b>{key}:</b>
+                {object[key]}
+            </p>
+            ))
+    }
+
     return (
         <>
-        
+
             <h1>Info</h1>
-            {user&&
-            <span>
-                <b>{user.name}</b>
-                <br />
-               
-                username : {user.username}   <br />
-                
-                email : {user.email}  <br />
-                phone : {user.phone} <br />
-                website : {user.website} <br />
-                <br />
-                address :
-                <br />
-                street: {user.address.street} <br />
-                suite: {user.address.suite} <br />
-                city: {user.address.city} <br />
-                zipcode: {user.address.zipcode} <br />
-                <br />
-                geo: 
-                <br />
-                lat: {user.address.geo.lat} <br />
-                lng: {user.address.geo.lng} <br />
-                <br />
-                company: 
-                <br />
-                name: {user.company.name} <br />
-                catchPhrase: {user.company.catchPhrase} <br />
-                bs: {user.company.bs} <br />
-            </span>}
+            {user &&
+                <span>
+                    {printUser(user)}
+                </span>}
         </>
     )
 }
